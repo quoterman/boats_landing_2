b103528f4be8d4be2f7fe1bf37333e4993778a8f
Sergey Zhilkin <szhilkin@gmail.com>
2016-02-15 11:53:04 +0300

    Unicorn config

---
+++ config/unicorn/production.rb
@@ -0,0 +1,41 @@
+worker_processes 3
+working_directory "/var/www/boats_landing/current" # available in 0.94.0+
+
+listen "0.0.0.0:80", :tcp_nopush => true
+
+timeout 60
+
+pid "/var/www/boats_landing/tmp/pids/unicorn.pid"
+
+stderr_path "/var/www/boats_landing/logs/unicorn.stderr.log"
+stdout_path "/var/www/boats_landing/logs/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # per-process listener ports for debugging/admin/migrations
+  # addr = "127.0.0.1:#{9293 + worker.nr}"
+  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+end
